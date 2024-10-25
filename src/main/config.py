e4378bbb88f5efbdfb42eed0bf98c88aeb5b1fb5
Marko Pejić <dev@markopejic.com>
2024-10-25 20:20:26 +0200

    Update files

--- src/main/config.py
+++ src/main/config.py
@@ -23,101 +23,98 @@ from PySide6.QtCore import QSettings
 class Config:
     def __init__(self):
         self.settings_manager = QSettings()
+
+        self.APP_NAME = "Media Downloader"
+        self.CONSTANT_SETTTINGS = {
+            "status_label_text": {
+                "download_failed": "Downloading Failed.",
+                "download_cancelled": "Downloading Cancelled.",
+                "download_finished": "Downloading Finished.",
+                "cancelling_download": "Cancelling Download...",
+                "downloading": "Downloading...",
+                "extracting_urls": "Analyzing URLs...",
+                "data_pull_failed": "Data Pull Failed.",
+                "data_pull_cancelled": "Data Pull Cancelled.",
+                "data_pull_finished": "Data Pull Finished.",
+                "cancelling_data_pull": "Cancelling Data Pull...",
+                "pulling_data": "Pulling Data...",
+                "converting": "Processing...",
+                "no_internet": "No internet connection.",
+            },
+
+            "status_label_icons": {
+                "download_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "download_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "download_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart)),
+                "cancelling_download": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "downloading": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
+                "extracting_urls": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
+                "data_pull_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "data_pull_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "data_pull_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogInformation)),
+                "cancelling_data_pull": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "pulling_data": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
+                "converting": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
+                "no_internet": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.NetworkOffline)),
+            },
+
+            "tab_text": {
+                "download_failed": "Failed",
+                "download_cancelled": "Cancelled",
+                "download_finished": "Finished",
+                "cancelling_download": "Cancelling",
+                "downloading": "Downloading",
+                "extracting_urls": "Analyzing",
+                "data_pull_failed": "Failed",
+                "data_pull_cancelled": "Cancelled",
+                "data_pull_finished": "Finished",
+                "cancelling_data_pull": "Cancelling",
+                "pulling_data": "Pulling Data",
+                "converting": "Downloading",
+                "no_internet": "Failed",
+            },
+
+            "tab_icons": {
+                "download_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "download_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "download_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart)),
+                "cancelling_download": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "downloading": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
+                "extracting_urls": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
+                "data_pull_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "data_pull_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "data_pull_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogInformation)),
+                "cancelling_data_pull": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+                "pulling_data": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
+                "converting": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
+                "no_internet": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
+            },
+
+            "window_titles": {
+                "error": "Error - Tab <pretty_tab_number>",
+            },
+
+            "button_text": {
+                "download": {"default": "&Download", "secondary": "Cancel &Download"},
+                "refresh": {"default": "&Pull Data", "secondary": "Cancel Data &Pull"},
+            },
+
+            "formats": {
+                "Audio": "mp3",
+                "Video": "mp4",
+            },
+        }
     
 
-    @classmethod
     def save_setting(self, setting, value):
         old_value = self.settings_manager.value(setting)
         if old_value != value:
             self.settings_manager.setValue(setting, value)
 
 
-    @classmethod
     def load_setting(self, setting):
         saved_value = self.settings_manager.value(setting)
         if saved_value != None:
             return saved_value
         else:
             return self.DEFAULT_SETTINGS[setting]
-    
-
-    APP_NAME = "Media Downloader"
-    CONSTANT_SETTTINGS = {
-        "status_label_text": {
-            "download_failed": "Downloading Failed.",
-            "download_cancelled": "Downloading Cancelled.",
-            "download_finished": "Downloading Finished.",
-            "cancelling_download": "Cancelling Download...",
-            "downloading": "Downloading...",
-            "extracting_urls": "Analyzing URLs...",
-            "data_pull_failed": "Data Pull Failed.",
-            "data_pull_cancelled": "Data Pull Cancelled.",
-            "data_pull_finished": "Data Pull Finished.",
-            "cancelling_data_pull": "Cancelling Data Pull...",
-            "pulling_data": "Pulling Data...",
-            "converting": "Processing...",
-            "no_internet": "No internet connection.",
-        },
-
-        "status_label_icons": {
-            "download_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "download_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "download_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart)),
-            "cancelling_download": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "downloading": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
-            "extracting_urls": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
-            "data_pull_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "data_pull_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "data_pull_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogInformation)),
-            "cancelling_data_pull": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "pulling_data": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
-            "converting": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
-            "no_internet": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.NetworkOffline)),
-        },
-
-        "tab_text": {
-            "download_failed": "Failed",
-            "download_cancelled": "Cancelled",
-            "download_finished": "Finished",
-            "cancelling_download": "Cancelling",
-            "downloading": "Downloading",
-            "extracting_urls": "Analyzing",
-            "data_pull_failed": "Failed",
-            "data_pull_cancelled": "Cancelled",
-            "data_pull_finished": "Finished",
-            "cancelling_data_pull": "Cancelling",
-            "pulling_data": "Pulling Data",
-            "converting": "Downloading",
-            "no_internet": "Failed",
-        },
-
-        "tab_icons": {
-            "download_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "download_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "download_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.MediaPlaybackStart)),
-            "cancelling_download": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "downloading": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
-            "extracting_urls": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
-            "data_pull_failed": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "data_pull_cancelled": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "data_pull_finished": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogInformation)),
-            "cancelling_data_pull": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-            "pulling_data": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ViewRefresh)),
-            "converting": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.GoDown)),
-            "no_internet": QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DialogWarning)),
-        },
-
-        "window_titles": {
-            "error": "Error - Tab <pretty_tab_number>",
-        },
-
-        "button_text": {
-            "download": {"default": "&Download", "secondary": "Cancel &Download"},
-            "refresh": {"default": "&Pull Data", "secondary": "Cancel Data &Pull"},
-        },
-
-        "formats": {
-            "Audio": "mp3",
-            "Video": "mp4",
-        },
-    }
